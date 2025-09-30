8d64c4e95a6824ab30bd9c5eabdaf45c77cfb6be
tim-akihiro <timakihiro@googlemail.com>
2025-09-30 14:24:12 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,1 @@
-import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
